5b4a4515b488017791f055fd1448b81d9ec245a3
win0908 <106888234+win0908@users.noreply.github.com>
2024-02-08 00:10:52 -0800

    Update FirstUniqueCharacterInString.py

--- Easy/FirstUniqueCharacterInString.py
+++ Easy/FirstUniqueCharacterInString.py
@@ -22,27 +22,30 @@ Output: -1
 '''
 
 
-def firstUniqChar(self, s):
-  """
-  :type s: str
-  :rtype: int
-  """
-  
-  if len(s) == 1 :
-    return 0
-  
-  d = {}
-  for i in s:
-    if i in d:
-      d[i] = d[i] + 1
-    else:
-      d[i] = 1
+def firstUniqChar(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    if len(s) == 1 :
+        return 0
+   
+    d = {}
+    for i in s:
+        if i in d:
+            d[i] = d[i] + 1
+        else:
+            d[i] = 1
       
     for i in range(len(s)):
-      if d[s[i]] == 1:
-        return i
+        if d[s[i]] == 1:
+            return i
       
-      return -1
+    return -1
+    
+
+
+
 
 
 
