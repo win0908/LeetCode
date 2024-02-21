27e0c011abfddb3c2043e9efb4a6ad339f3873d8
win0908 <106888234+win0908@users.noreply.github.com>
2024-02-21 04:32:17 -0800

    Update 0035-search-insert-position.py
    Time: O(n) -> O(log n)

--- Easy/0035-search-insert-position.py
+++ Easy/0035-search-insert-position.py
@@ -1,9 +1,30 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
+        '''
+        #Time: O(n)
         for i in range(len(nums)):
             if nums[i] == target: return i
             if nums[i] > target:  return i
 
         return len(nums)
+        '''
+
+        
+        #Time: O(log n)
+        #Space: O(1)
+        
+        l = 0
+        r = len(nums)
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m
+
+        return l
         
